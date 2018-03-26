6319d6c90bda6a238c9a84730d225e806535eb2e
Iskander Broere <iskanderbroere@gmail.com>
2018-03-26 21:55:27 +0200

    fix no array

--- utils/localstorage.js
+++ utils/localstorage.js
@@ -4,5 +4,9 @@ export const setLocalStorage = async items => {
   return await localforage.setItem("favorites", items.slice(0, 10))
 }
 export const getLocalStorage = async key => {
-  return await localforage.getItem(key)
+  const localData = await localforage.getItem(key)
+  if (localData.length) {
+    return localData
+  }
+  return []
 }
