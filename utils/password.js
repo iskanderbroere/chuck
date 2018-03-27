7eab8bc15f9675900e24d00450ad0faaf3a4ef7a
Iskander Broere <iskanderbroere@gmail.com>
2018-03-27 12:46:36 +0200

    increasing straight

--- utils/password.js
+++ utils/password.js
@@ -35,11 +35,44 @@ const alphabet = [
   "z"
 ]
 
+const indexOfAlphabet = letter => {
+  return alphabet.indexOf(letter)
+}
+
+const isIncreasingStraight = (first, second, third) => {
+  const firstAlphabetIndex = indexOfAlphabet(first)
+  const secondAlphabetIndex = indexOfAlphabet(second)
+  const thirdAlphabetIndex = indexOfAlphabet(third)
+  console.log(firstAlphabetIndex, secondAlphabetIndex, thirdAlphabetIndex)
+  const sumOne = secondAlphabetIndex - firstAlphabetIndex === 1
+  const sumTwo = thirdAlphabetIndex - secondAlphabetIndex === 1
+  return sumOne && sumTwo
+}
+
 export const tooLong = password => password.length > 32
+
 export const tooShort = password => password.length < 3
+
 export const isLowerCaseAlphabetic = password => !password.match(/^[a-z]+$/)
+
 export const hasIllegalCharacters = password => !password.match(/^[i]+$/)
 
+export const hasIncreasingStraight = password => {
+  const passwordArray = passwordToArray(password)
+  const passwordLength = passwordArray.length - 3
+  for (let n = 0; n <= passwordLength; n++) {
+    const firstLetter = passwordArray[n]
+    const secondLetter = passwordArray[n + 1]
+    const thirdLetter = passwordArray[n + 2]
+    if (isIncreasingStraight(firstLetter, secondLetter, thirdLetter)) {
+      console.log("INCREASING STRAIGHT FOUND")
+      return true
+    }
+    if (n === passwordLength) {
+      return false
+    }
+  }
+}
 export const validatePassword = password => {
   const trimmedPassword = trimPassword(password)
   if (tooLong(trimmedPassword)) {
